2f26f27103faa30ba413cb40b2e87f1ecb3eb869
Andrew Peterson <cybernetico@gmail.com>
2017-01-07 12:04:09 -0500

    Input changes.

--- NesVoxelLib/Input.cpp
+++ NesVoxelLib/Input.cpp
@@ -3,99 +3,101 @@
 #include "NesEmulator.hpp"
 #include "libretro.h"
 
-ControllerState::ControllerState()
+void DigitalInput::update()
 {
+	if (active)
+	{
+		if (framesActive > 0)
+			setActivatedThisFrame(false);
+		else
+			setActivatedThisFrame(true);
+		setFramesActive(framesActive + 1);
+	}
+	else
+	{
+		setActivatedThisFrame(false);
+		setFramesActive(0);
+	}
 }
 
-ControllerState::ControllerState(XINPUT_GAMEPAD gamepad)
+float DigitalInput::getValue()
+{
+	if (active)
+		return 1.0f;
+	else
+		return 0.0f;
+}
+
+void AnalogInput::update()
+{
+	if (value > deadzone)
+	{
+		if (framesActive > 0)
+			setActivatedThisFrame(false);
+		else
+			setActivatedThisFrame(true);
+		setFramesActive(framesActive + 1);
+		setActive(true);
+	}
+	else
+	{
+		setActive(false);
+		setActivatedThisFrame(false);
+		setFramesActive(0);
+	}
+}
+
+float AnalogInput::getValue()
+{
+	return 0.0f;
+}
+
+KeyboardMouseDevice::KeyboardMouseDevice()
+{
+	for (int i = 0; i < totalKeys; i++)
+	{
+		keys[i] = make_shared<DigitalInput>();
+	}
+}
+
+void KeyboardMouseDevice::setDown(int key)
+{
+	keys[key]->setActive(true);
+}
+
+void KeyboardMouseDevice::setUp(int key)
+{
+	keys[key]->setActive(false);
+}
+
+void KeyboardMouseDevice::update()
+{
+	for (int i = 0; i < totalKeys; i++)
+	{
+		keys[i]->update();
+	}
+}
+
+void GamepadDevice::update(XINPUT_GAMEPAD gamepad)
 {
 	// Read buttons
-	buttonStates[ba] = ((gamepad.wButtons & XINPUT_GAMEPAD_A) != 0);
-	buttonStates[bb] = ((gamepad.wButtons & XINPUT_GAMEPAD_B) != 0);
-	buttonStates[bx] = ((gamepad.wButtons & XINPUT_GAMEPAD_X) != 0);
-	buttonStates[by] = ((gamepad.wButtons & XINPUT_GAMEPAD_Y) != 0);
-	buttonStates[bstart] = ((gamepad.wButtons & XINPUT_GAMEPAD_START) != 0);
-	buttonStates[bselect] = ((gamepad.wButtons & XINPUT_GAMEPAD_BACK) != 0);
-	buttonStates[bdLeft] = ((gamepad.wButtons & XINPUT_GAMEPAD_DPAD_LEFT) != 0);
-	buttonStates[bdRight] = ((gamepad.wButtons & XINPUT_GAMEPAD_DPAD_RIGHT) != 0);
-	buttonStates[bdUp] = ((gamepad.wButtons & XINPUT_GAMEPAD_DPAD_UP) != 0);
-	buttonStates[bdDown] = ((gamepad.wButtons & XINPUT_GAMEPAD_DPAD_DOWN) != 0);
-	buttonStates[blb] = ((gamepad.wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER) != 0);
-	buttonStates[brb] = ((gamepad.wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER) != 0);
+	buttons[ba]->setActive(((gamepad.wButtons & XINPUT_GAMEPAD_A) != 0));
+	buttons[bb]->setActive(((gamepad.wButtons & XINPUT_GAMEPAD_B) != 0));
+	buttons[bx]->setActive(((gamepad.wButtons & XINPUT_GAMEPAD_X) != 0));
+	buttons[by]->setActive(((gamepad.wButtons & XINPUT_GAMEPAD_Y) != 0));
+	buttons[bstart]->setActive(((gamepad.wButtons & XINPUT_GAMEPAD_START) != 0));
+	buttons[bselect]->setActive(((gamepad.wButtons & XINPUT_GAMEPAD_BACK) != 0));
+	buttons[bdLeft]->setActive(((gamepad.wButtons & XINPUT_GAMEPAD_DPAD_LEFT) != 0));
+	buttons[bdRight]->setActive(((gamepad.wButtons & XINPUT_GAMEPAD_DPAD_RIGHT) != 0));
+	buttons[bdUp]->setActive(((gamepad.wButtons & XINPUT_GAMEPAD_DPAD_UP) != 0));
+	buttons[bdDown]->setActive(((gamepad.wButtons & XINPUT_GAMEPAD_DPAD_DOWN) != 0));
+	buttons[blb]->setActive(((gamepad.wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER) != 0));
+	buttons[brb]->setActive(((gamepad.wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER) != 0));
 	// Read analog sticks
-	analogStickStates[lStick] = { gamepad.sThumbLX / 32767.0f, gamepad.sThumbLY / 32767.0f };
-	analogStickStates[rStick] = { gamepad.sThumbRX / 32767.0f, gamepad.sThumbRY / 32767.0f };
-	// Read triggers
-	triggerStates[lTrigger] = gamepad.bLeftTrigger / 255.0f;
-	triggerStates[rTrigger] = gamepad.bRightTrigger / 255.0f;
-	// Dead-zone sticks and triggers
-	if (analogStickStates[lStick].x < 0.15f && analogStickStates[lStick].x > -0.15f)
-		analogStickStates[lStick].x = 0;
-	if (analogStickStates[lStick].y < 0.15f && analogStickStates[lStick].y > -0.15f)
-		analogStickStates[lStick].y = 0;
-	if (analogStickStates[rStick].x < 0.15f && analogStickStates[rStick].x > -0.15f)
-		analogStickStates[rStick].x = 0;
-	if (analogStickStates[rStick].y < 0.15f && analogStickStates[rStick].y > -0.15f)
-		analogStickStates[rStick].y = 0;
-}
-
-InputState::InputState()
-{
-	keyboardState = KeyboardState();
-}
-
-void InputState::checkGamePads()
-{
-	XINPUT_STATE state;
-	// See if either controller is connected and grab input state if so
-	if (XInputGetState(0, &state) == ERROR_SUCCESS)
-	{
-		connected[0] = true;
-		gamePads[0] = ControllerState(state.Gamepad);
-	}
-	else
-	{
-		connected[0] = false;
-	}
-	if (XInputGetState(1, &state) == ERROR_SUCCESS)
-	{
-		connected[1] = true;
-		gamePads[1] = ControllerState(state.Gamepad);
-	}
-	else
-	{
-		connected[1] = false;
-	}
-}
-
-void InputState::refreshInput()
-{
-	// Send to emulator
-	NesEmulator::inputs[0][RETRO_DEVICE_ID_JOYPAD_A] = ((connected[0] && gamePads[0].buttonStates[ba]) || keyboardState.keyStates[75]);;
-	NesEmulator::inputs[0][RETRO_DEVICE_ID_JOYPAD_B] = ((connected[0] && gamePads[0].buttonStates[bx]) || keyboardState.keyStates[74]);;
-	NesEmulator::inputs[0][RETRO_DEVICE_ID_JOYPAD_SELECT] = ((connected[0] && gamePads[0].buttonStates[bselect]) || keyboardState.keyStates[85]);
-	NesEmulator::inputs[0][RETRO_DEVICE_ID_JOYPAD_START] = ((connected[0] && gamePads[0].buttonStates[bstart]) || keyboardState.keyStates[73]);
-	NesEmulator::inputs[0][RETRO_DEVICE_ID_JOYPAD_UP] = ((connected[0] && gamePads[0].buttonStates[bdUp]) || keyboardState.keyStates[87]);;
-	NesEmulator::inputs[0][RETRO_DEVICE_ID_JOYPAD_DOWN] = ((connected[0] && gamePads[0].buttonStates[bdDown]) || keyboardState.keyStates[83]);;
-	NesEmulator::inputs[0][RETRO_DEVICE_ID_JOYPAD_LEFT] = ((connected[0] && gamePads[0].buttonStates[bdLeft]) || keyboardState.keyStates[65]);;
-	NesEmulator::inputs[0][RETRO_DEVICE_ID_JOYPAD_RIGHT] = ((connected[0] && gamePads[0].buttonStates[bdRight]) || keyboardState.keyStates[68]);;
-	
-}
-
-KeyboardState::KeyboardState()
-{
-	for (int i = 0; i < 256; i++)
-	{
-		keyStates[i] = false;
-	}
-}
-
-void KeyboardState::setDown(int key)
-{
-	keyStates[key] = true;
-}
-
-void KeyboardState::setUp(int key)
-{
-	keyStates[key] = false;
+	analogInputs[leftX]->value = gamepad.sThumbLX / 32767.0f;
+	analogInputs[leftY]->value = gamepad.sThumbLY / 32767.0f;
+	analogInputs[rightX]->value = gamepad.sThumbRX / 32767.0f;
+	analogInputs[rightY]->value = gamepad.sThumbRY / 32767.0f;
+	analogInputs[lTrigger]->value = gamepad.bLeftTrigger / 255.0f;
+	analogInputs[rTrigger]->value = gamepad.bRightTrigger / 255.0f;
 }
--- NesVoxelLib/Input.hpp
+++ NesVoxelLib/Input.hpp
@@ -4,46 +4,102 @@
 
 #pragma comment(lib, "XInput.lib") // XInput library
 
-enum nesButtons {
+using namespace std;
 
+const int totalKeys = 256;
+const int xboxButtonCount = 14;
+const int xboxAxisCount = 6;
+
+enum inputFunctions {
+	nes_p1_a, nes_p1_b, nes_p1_up, nes_p1_left, nes_p1_down, nes_p1_right, nes_p1_start, nes_p1_select,
+	nes_p2_a, nes_p2_b, nes_p2_up, nes_p2_left, nes_p2_down, nes_p2_right, nes_p2_start, nes_p1_select,
+	emu_pause, emu_reset, LAST
 };
+
+enum device { keyboardMouse, gamepad1, gamepad2 };
 
 enum buttonNames { bdUp, bdDown, bdLeft, bdRight, ba, bb, bx, by, blb, brb, bselect, bstart, blClick, brClick };
-enum triggerNames { lTrigger, rTrigger };
-enum stickNames { lStick, rStick };
+enum axisNames { leftX, leftY, rightX, rightY, lTrigger, rTrigger };
 
-struct AnalogStickState
-{
-	float x;
-	float y;
-};
-
-class ControllerState
+class Input
 {
 public:
-	ControllerState();
-	ControllerState(XINPUT_GAMEPAD gamepad);
-	bool buttonStates[14];
-	AnalogStickState analogStickStates[2];
-	float triggerStates[2];
+	bool active = false;
+	int framesActive = 0;
+	bool activatedThisFrame = false;
+	// Jesus Christ, C++ polymorphism is the worst
+	void setActive(bool a) { active = a; }
+	void setFramesActive(int a) { framesActive = a; }
+	void setActivatedThisFrame(bool a) { activatedThisFrame = a; }
+	virtual float getValue() = 0;
+	virtual void update() = 0;
 };
 
-class KeyboardState
+class DigitalInput : public Input
 {
 public:
-	KeyboardState();
-	bool keyStates[256];
+	void update();
+	float getValue();
+};
+
+class AnalogInput : Input
+{
+public:
+	float value = 0.0f;
+	float deadzone = 0.1f;
+	void update();
+	float getValue();
+};
+
+class InputFunction
+{
+	vector<Binding> bindings;
+	bool active;
+	int activeTime;
+};
+
+class InputDevice
+{
+	virtual DigitalInput getDigitalInput(int inputNumber) = 0;
+	virtual AnalogInput getAnalogInput(int inputNumber) = 0;
+};
+
+class KeyboardMouseDevice : InputDevice
+{
+	KeyboardMouseDevice();
+	float mouseX;
+	float mouseY;
+	shared_ptr<DigitalInput> keys[totalKeys];
 	void setDown(int key);
 	void setUp(int key);
+	void update();
+};
+
+class GamepadDevice : InputDevice
+{
+	bool connected = false;
+	shared_ptr<DigitalInput> buttons[xboxButtonCount];
+	shared_ptr<AnalogInput> analogInputs[xboxAxisCount];
+	void update(XINPUT_GAMEPAD gamepad);
+};
+
+class Binding
+{
+	shared_ptr<Input> input;
+	bool ctrl = false;
+	bool alt = false;
+	bool shift = false;
 };
 
 class InputState
 {
 public:
 	InputState();
+	shared_ptr<KeyboardMouseDevice> keyboardMouse;
+	shared_ptr<GamepadDevice> gamepads[2];
+	shared_ptr<Binding> bindings[inputFunctions::LAST];
 	bool connected[2];
-	ControllerState gamePads[2];
-	KeyboardState keyboardState;
+	
 	void checkGamePads();
 	void refreshInput();
 };
